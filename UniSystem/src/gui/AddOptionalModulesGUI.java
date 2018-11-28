e4ed00ee9ee7c7cd1dced6f31efc89077236bcd9
hube24 <hb12@o2.pl>
2018-11-28 21:58:33 +0000

    add optional modules gui

---
+++ UniSystem/src/gui/AddOptionalModulesGUI.java
@@ -0,0 +1,46 @@
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import database.Session;
+import users.Student;
+
+public class AddOptionalModulesGUI extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AddOptionalModulesGUI frame = new AddOptionalModulesGUI(null,null);
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public AddOptionalModulesGUI(Session s, Student student) {
+		Session currSession = s;
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 698, 461);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+	}
+
+}

08cdb6fe9c7c428f729ed1c965d0a8540c0c09dc
acb17hb <acb17hb@TENB885849FDEA9.shefuniad.shef.ac.uk>
2018-11-28 19:24:36 +0000

    Added Module class

--- UniSystem/src/university/Module.java
+++ UniSystem/src/university/Module.java
@@ -2,4 +2,25 @@ package university;
 
 public class Module {
 
+	String codeOfModule;
+	String name;
+	int numberOfCredits;
+	
+	public Module(String code, String n, int credits) {
+		codeOfModule = code;
+		name = n;
+		numberOfCredits = credits;
+	}
+	
+	public String getCodeOfModule() {
+		return codeOfModule;
+	}
+	public String getName() {
+		return name;
+	}
+	public int getNumberOfCredits() {
+		return numberOfCredits;
+	}
+	
+	
 }

feeaf0f0512fe3db77dfc113e9ab820b6ef0691e
Hubert <hb12@o2.pl>
2018-12-04 23:03:30 +0100

    partly finished teacher class (anyone please finish it if you have time)

--- UniSystem/src/users/Teacher.java
+++ UniSystem/src/users/Teacher.java
@@ -1,4 +1,14 @@
 package users;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.List;
+
+import javax.swing.JOptionPane;
+
+import database.DatabaseSelector;
+import database.SqlDriver;
 import university.Module;
 
 public class Teacher extends User {
@@ -13,6 +23,10 @@ public class Teacher extends User {
 		forename = f;
 	}
 	
+	public static void infoBox(String infoMessage, String titleBar) {
+		JOptionPane.showMessageDialog(null, infoMessage, titleBar, JOptionPane.INFORMATION_MESSAGE);
+	}
+	
 	public String getTitle() {
 		return title;
 	}
@@ -25,5 +39,263 @@ public class Teacher extends User {
 		return forename;
 	}
 	
+	boolean addGrade(int grade, int regNum, String moduleCode, String fstsnd )
+	{
+		SqlDriver sqldriver = new SqlDriver();
+		
+		try (Connection con = DriverManager.getConnection(sqldriver.getDB(), sqldriver.getDBuser(), sqldriver.getDBpassword())) {
+			PreparedStatement pst1;
+			if(fstsnd == "first")
+				pst1= con.prepareStatement("UPDATE ModuleRegistration SET firstGrade = ? WHERE codeOfModule = ? AND registrationNum = ?"); //toedit
+			else
+				pst1= con.prepareStatement("UPDATE ModuleRegistration SET secondGrade = ? WHERE codeOfModule = ? AND registrationNum = ?"); //toedit
+			
+			pst1.setInt(1, grade);
+			pst1.setString(2, moduleCode);
+			pst1.setInt(3, regNum);
+			pst1.executeUpdate();
+			con.close();
+			return true;
+		} catch (Exception exc) {
+			exc.printStackTrace();
+			return false;
+		}
+		
+	}
+	
+	//returns P if passed, C if conceded pass, F if fail
+	String moduleGradeState(int grade, String lvl)
+	{
+		int pass=0;
+		
+		if(lvl == "1" || lvl == "2" || lvl == "3") pass = 40;
+		if(lvl == "4") pass = 50;
+		
+		if(grade>=pass)return "P";
+		if(grade>=(pass-10))return "C";
+		return "F";
+	}
+	
+	//returns P if passed, C if conceded pass, F if fail
+	String modulePassed(Student student, Module module)
+	{
+		String level = student.getCurrentLevel();
+		
+		SqlDriver sqldriver = new SqlDriver();
+		
+		try (Connection con = DriverManager.getConnection(sqldriver.getDB(), sqldriver.getDBuser(), sqldriver.getDBpassword())) {
+			PreparedStatement pst1 = con.prepareStatement("SELECT firstGrade, secondGrade FROM ModuleRegistration"
+					+ "WHERE registrationNum = ? AND codeOfModule = ?");
+			
+			pst1.setInt(1, student.getRegistrationID());
+			pst1.setString(2, module.getCodeOfModule());
+			ResultSet rs = pst1.executeQuery();
+			if(rs.next())
+			{
+				int fstGrade = rs.getInt(1);
+				
+				if(rs.getObject(2)==null) {
+					return moduleGradeState(fstGrade, level);
+				}else {
+					int sndGrade = rs.getInt(2);
+					return moduleGradeState(sndGrade, level);
+				}
+			}
+			con.close();
+		} catch (Exception exc) {
+			exc.printStackTrace();
+			return "";
+		}
+		return "";
+	}
+	
+	
+	String nextLevel(Student student)
+	{
+		String level = student.getCurrentLevel();
+		int numOfLevels = student.getDegree().getNumberOfLevels();
+		if(numOfLevels>1)
+		{
+			if(student.getDegree().getName().endsWith("with a Year of Industry")  && level == "2")
+			{
+				return "P";
+			}
+			
+			if(level == "1") return "2";
+			if(level == "2") return "3";
+			if(level == "3" && numOfLevels == 4) return "4";
+			if(level == "3" && numOfLevels == 3) return "graduation";
+			if(level == "4" && numOfLevels == 4) return "graduation";
+			
+		}else {
+			return "graduation";
+		}
+		return "";
+	}
+	
+	String nextPeriod(String period)
+	{
+		char p = period.charAt(0);
+		p++;
+		return String.valueOf(p);
+	}
+	
+	boolean setGraduationGrade(Student student)
+	{
 
+		SqlDriver sqldriver = new SqlDriver();
+		
+		try (Connection con = DriverManager.getConnection(sqldriver.getDB(), sqldriver.getDBuser(), sqldriver.getDBpassword())) {
+			
+			
+			String setGraduationGrade = "UPDATE Student SET graduationGrade = ? WHERE registrationNum = ?";
+			PreparedStatement pst3 = con.prepareStatement(setGraduationGrade);
+			
+			String degreeName[] = student.getDegree().getName().split(" ",2);
+			String degreeType = degreeName[0]; //MEng, MSc, BSc, BEng etc.
+			String graduationGrade = graduationGradeState(finalGrade, degreeType);
+			
+			pst3.setString(1,finalGrade);
+			pst3.setString(2, currPeriod);
+			pst3.setInt(3, student.getRegistrationID());
+		}catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+	
+	String progressToNextLevel(Student student, int finalGrade)
+	{
+		
+		String nextLevel = nextLevel(student);
+		String currPeriod = student.getCurrentPeriodOfStudy();
+		String nextPeriod = nextPeriod(currPeriod);
+		
+		SqlDriver sqldriver = new SqlDriver();
+		
+		try (Connection con = DriverManager.getConnection(sqldriver.getDB(), sqldriver.getDBuser(), sqldriver.getDBpassword())) {
+
+			//set current period final grade
+			String setPeriodFinalGrade = "UPDATE StudentStudyPeriod SET finalGrade = ? WHERE label = ? AND registrationNum = ?";
+			PreparedStatement pst1 = con.prepareStatement(setPeriodFinalGrade);
+			pst1.setInt(1,finalGrade);
+			pst1.setString(2, currPeriod);
+			pst1.setInt(3, student.getRegistrationID());
+			
+			if(nextLevel != "graduation") {
+				String insertStuPerQ = "INSERT INTO StudentStudyPeriod (registrationNum, label, level)" + "VALUES (?, ?, ?)";
+				PreparedStatement pst2 = con.prepareStatement(insertStuPerQ);
+				pst2.setInt(1, student.getRegistrationID());
+				pst2.setString(2, nextPeriod);
+				pst2.setString(3, nextLevel);
+				pst2.executeUpdate();
+			}else {
+				//
+			}
+			con.close();
+			
+		} catch (Exception exc) {
+			
+			infoBox("Student could not be progressed.", "Warning");
+			exc.printStackTrace();
+		}
+		
+		
+		return "";
+	}
+	
+	// return if student is able to progress, count final grade for period of study
+	boolean ableToProgress(Student student)
+	{
+		// 1 - check all modules
+		// 2 - count number of obtained credits
+		// 3 - count final grade
+		// 4 - set final grade for period of study
+		// 5 - return if student is able to progress
+		String level = student.getCurrentLevel();
+		int modulesPassed = 0;
+		int modulesFailed = 0;
+		int concededPasses = 0;
+		int creditsSum = 0;
+		
+		DatabaseSelector dbSelector = new DatabaseSelector();
+		List<String[]> modulesList = dbSelector.getRegisteredModules(student);
+		
+		for(String[] row : modulesList)
+		{
+			String codeOfModule = row[0];
+			String name = row[1];
+			int credits = Integer.valueOf(row[2]);
+			int registrationNum = Integer.valueOf(row[3]);
+			
+			if(row[5]==null && row[6] == null) {
+				infoBox("Module "+ codeOfModule + " is not marked yet, please set grades for this module.","Warning");
+				return false;
+			}
+			
+			int firstGrade;
+			int secondGrade;
+			
+			if(row[6] == null) {
+				firstGrade = Integer.valueOf(row[5]);
+				if(moduleGradeState(firstGrade, level)=="P") 
+				{
+					modulesPassed++;
+					creditsSum+=credits;
+				}
+				if(moduleGradeState(firstGrade, level)=="C") 
+				{
+					concededPasses++;
+				}
+				if(moduleGradeState(firstGrade, level)=="F") 
+				{
+					modulesFailed++;
+					infoBox("Module "+ codeOfModule + " was failed at the first attempt, please set retake grade.","Warning");
+					return false;
+				}
+				
+			}else {
+				secondGrade = Integer.valueOf(row[6]);
+				if(moduleGradeState(secondGrade, level)=="P") 
+				{
+					modulesPassed++;
+					creditsSum+=credits;
+				}
+				if(moduleGradeState(secondGrade, level)=="C") 
+				{
+					concededPasses++;
+				}
+				if(moduleGradeState(secondGrade, level)=="F") 
+				{
+					modulesFailed++;
+					infoBox("Module "+ codeOfModule + " was failed at the second attempt, student is unable to progress","Warning");
+					return false;
+				}
+			}
+			Module module = new Module(codeOfModule, name, credits);
+		}
+		
+		int creditsToObtain = (level=="4")?180:120;
+		
+		if(level == "4") {
+			if(creditsToObtain - creditsSum < 15 && modulesFailed==0 && concededPasses<=1)
+			{
+				//passed
+				return true;
+			}else {
+				//failed
+				return false;
+			}
+		}else {
+			if(creditsToObtain - creditsSum < 20 && modulesFailed==0  && concededPasses<=1)
+			{
+				//passed
+				return true;
+			}else {
+				
+				//failed
+				return false;
+			}
+		}
+	}
+	
 }

3196912bc95eb87e316e0058899246967ea425b3
ziyuema <44809942+ziyuema@users.noreply.github.com>
2018-12-04 02:43:25 +0000

    Create CheckStudent.java

---
+++ UniSystem/src/gui/CheckStudent.java
@@ -0,0 +1,75 @@
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class CheckStudent extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					CheckStudent frame = new CheckStudent();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public CheckStudent() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 500, 500);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnDetail = new JButton("Detail");
+		btnDetail.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnDetail.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				setVisible(false);
+				CheckStudentStatus newFrame = new CheckStudentStatus();
+				newFrame.setVisible(true);
+			}
+		});
+		btnDetail.setBounds(100, 401, 89, 23);
+		contentPane.add(btnDetail);
+		
+		JButton btnBack = new JButton("Back");
+		btnBack.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				setVisible(false);
+				TeacherGUI newFrame = new TeacherGUI(null);
+				newFrame.setVisible(true);
+			}
+		});
+		btnBack.setBounds(277, 401, 89, 23);
+		contentPane.add(btnBack);
+	}
+
+}
